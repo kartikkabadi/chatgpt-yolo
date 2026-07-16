6185c62a955c5f24f4850ab26394d807629d118c
Kartik <133712749+kartikkabadi@users.noreply.github.com>
2026-07-16 21:23:33 +0530

    Restore the extension runtime allowlist

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -7,7 +7,6 @@ export const RUNTIME_FILES = Object.freeze([
   "background-wrapper.js",
   "tab-supervisor.js",
   "background.js",
-  "counter-background.js",
   "data-background.js",
   "portability.js",
   "config.js",
