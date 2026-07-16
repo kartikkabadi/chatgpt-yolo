4cfeef6436f94ff1dae4a6f9132c68c129adf0f9
Kartik <133712749+kartikkabadi@users.noreply.github.com>
2026-07-16 21:23:16 +0530

    Restore the background worker entry points

--- background-wrapper.js
+++ background-wrapper.js
@@ -1,3 +1,3 @@
 "use strict";
 
-importScripts("lifecycle.js", "background.js", "counter-background.js", "portability.js", "data-background.js", "tab-supervisor.js");
+importScripts("lifecycle.js", "background.js", "portability.js", "data-background.js", "tab-supervisor.js");
